707038ef88bcc8bf5a155da1f8d552e2feb61686
Justin Yoo <justin.yoo@aliencube.com>
2014-04-19 21:32:19 +1000

    Updated multiplication up to 12x table.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,15 @@
 ;"use strict";
 
 (function($) {
-    var getNumber = function() {
-        var max = 99;
-        var min = 1;
+    var getNumber = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
     var loadFormula = function() {
         var calcType = $("#menu li.active").text();
 
-        var number1 = getNumber();
-        var number2 = getNumber();
+        var number1 = getNumber(1, 99);
+        var number2 = getNumber(1, 99);
         var symbol = "";
         var solution = 0;
 
@@ -23,20 +21,24 @@
             case "Substraction":
                 symbol = "-";
                 while (number1 <= number2) {
-                    number1 = getNumber();
-                    number2 = getNumber();
+                    number1 = getNumber(1, 99);
+                    number2 = getNumber(1, 99);
                 }
                 solution = number1 - number2;
                 break;
             case "Multiplication":
                 symbol = "&times;";
+                while (number1 > 12 || number2 > 12) {
+                    number1 = getNumber(1, 12);
+                    number2 = getNumber(1, 12);
+                }
                 solution = number1 * number2;
                 break;
             case "Division":
                 symbol = "&divide;";
                 while (Math.floor(number1 / number2) != number1 / number2) {
-                    number1 = getNumber();
-                    number2 = getNumber();
+                    number1 = getNumber(1, 99);
+                    number2 = getNumber(1, 99);
                 }
                 solution = number1 / number2;
                 break;
